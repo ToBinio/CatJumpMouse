ee6f0ee0f71489b714750491c093104c3cd7b5e6
benni043 <benni.huff@gmail.com>
2023-02-10 17:24:09 +0100

    test commit

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -1,7 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class HelloWorld : MonoBehaviour
 {
+    [SerializeField] private string testVar = "Hello World";
+    public void Start()
+    {
+        Debug.Log(testVar);
+    }
 }
